7ef3f6a6d6d7021483a6569d2ab9e690423c54fa
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-01-11 23:03:06 -0300

    http and restrusture the folders

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -14,11 +14,17 @@ export class CourseInfoComponent implements OnInit{
 
     }
     
-    ngOnInit(): void {
-        this.course = this.courseService.retriveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+    ngOnInit(): void { 
+        this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe({
+            next: course => this.course = course,
+            error: err => console.log('Error', err)
+        });
     }
 
     save(): void {
-        this.courseService.save(this.course);
+        this.courseService.save(this.course).subscribe({
+            next: course => console.log('Saved with success', course),
+            error: err => console.log('Error', err)
+        });
     }
 }
